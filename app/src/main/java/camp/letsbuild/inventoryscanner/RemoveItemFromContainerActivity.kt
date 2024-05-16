02cca0f788f32143bbc3d7fac637e80f1bf377bb
Thejas Elandassery <58774753+Thejas775@users.noreply.github.com>
2024-05-17 03:19:09 +0530

    Add Cancel Button to RemoveItemFromContainerActivity

--- app/src/main/java/camp/letsbuild/inventoryscanner/RemoveItemFromContainerActivity.kt
+++ app/src/main/java/camp/letsbuild/inventoryscanner/RemoveItemFromContainerActivity.kt
@@ -65,6 +65,10 @@ class RemoveItemFromContainerActivity : ComponentActivity() {
                 }) }) {
                     Text("Confirm Remove Item")
                 }
+                // Adding a Cancel button as per plan
+                Button(onClick = { finish() }) {
+                    Text("Cancel")
+                }
             }
         }
     }

c5b738a7c8ae9482118b2d74123c5af6d1d2b6a4
Brendan Higgins <bhig93@gmail.com>
2024-06-04 01:01:58 -0400

    Add a top title bar to the ToolshedManager app

--- toolshedmanager/src/main/java/camp/letsbuild/toolshedmanager/MainActivity.kt
+++ toolshedmanager/src/main/java/camp/letsbuild/toolshedmanager/MainActivity.kt
@@ -6,13 +6,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -38,25 +42,30 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ToolshedManagerUI(componentActivity: ComponentActivity, modifier: Modifier = Modifier
     .fillMaxSize()
     .wrapContentSize(Alignment.Center)) {
-    Column(
-        modifier = modifier.verticalScroll(rememberScrollState()),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Button(onClick = {
-            val intent = Intent(componentActivity, ToolshedCheckoutActivity::class.java)
-            componentActivity.startActivity(intent)
-        }) {
-            Text("Tool Shed - Check-Out")
-        }
-        Button(onClick = {
-            val intent = Intent(componentActivity, ToolshedCheckinActivity::class.java)
-            componentActivity.startActivity(intent)
-        }) {
-            Text("Tool Shed - Check-In")
+    Scaffold(topBar = {
+        TopAppBar(title = { Text("Toolshed Manager") })
+    }) { padding ->
+        Column(
+            modifier = modifier.verticalScroll(rememberScrollState()).padding(padding),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(onClick = {
+                val intent = Intent(componentActivity, ToolshedCheckoutActivity::class.java)
+                componentActivity.startActivity(intent)
+            }) {
+                Text("Tool Shed - Check-Out")
+            }
+            Button(onClick = {
+                val intent = Intent(componentActivity, ToolshedCheckinActivity::class.java)
+                componentActivity.startActivity(intent)
+            }) {
+                Text("Tool Shed - Check-In")
+            }
         }
     }
 }
